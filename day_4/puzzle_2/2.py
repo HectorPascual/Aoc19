060d3e6ac918616df47969ae69a3720bba5e905d
pascual <hector.pascual@silicon-gears.com>
2019-12-23 10:46:25 +0100

    Day 4 complete

---
+++ day_4/puzzle_2/2.py
@@ -0,0 +1,34 @@
+def check_increase(num):
+    """
+    Checks that going from left to right, the digits never decrease
+    :param num:
+    :return: True if the num match the condition or false if it decreases
+    """
+    prev = 0
+    for i in str(num):
+        if int(i) >= prev:
+            prev = int(i)
+        else:
+            return False
+    return True
+
+
+def check_adjacency(num):
+    """
+    Checks that two adjacent digits are the same (like 22 in 122345) but are not part of a larger
+    group, only two.
+    :param num:
+    :return:
+    """
+    for i in str(num):
+        if str(num).count(i) == 2 and str(num)[str(num).index(i)+1] == i:
+            return True
+    return False
+
+
+def count_passwords(array):
+    return len(list(filter(lambda x: check_adjacency(x) and check_increase(x), array)))
+
+
+input = [i for i in range(273025, 767253)]
+print(count_passwords(input))
---
+++ day_5/puzzle_1/1.py
@@ -0,0 +1,17 @@
+def calc_final_state(input):
+    """
+    Iterates length/4 times through the array, taking the first position of the group of 4 as the
+    op code, the second and third position values as the index for the parameters of the operation
+    and the last position shows the index where the result will be stored.
+    :param input:
+    :return: the final state of the input array
+    """
+    for i in range(0,int(len(input)/4)+1):
+        if input[0+i*4] == 1:
+            input[input[3+i*4]] = input[input[1+i*4]] + input[input[2+i*4]]
+        elif input[0+i*4] == 2:
+            input[input[3+i*4]] = input[input[1+i*4]] * input[input[2+i*4]]
+        elif input[0+i*4] == 99:
+            break
+    return input
+
